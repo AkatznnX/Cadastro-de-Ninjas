02fa29d115b7ac4fe14f93dd3f8788d03b7a514d
AkatznnX <akateff326@gmail.com>
2026-03-07 07:42:51 -0300

    chore:NinjaDTO and NinjaMapper are Working now.

--- demo/src/main/java/com/example/demo/Ninjas/NinjaDTO.java
+++ demo/src/main/java/com/example/demo/Ninjas/NinjaDTO.java
@@ -1,4 +1,20 @@
 package com.example.demo.Ninjas;
 
+import com.example.demo.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String rank;
+    private MissoesModel missoes;
+
 }
--- demo/src/main/java/com/example/demo/Ninjas/NinjaMapper.java
+++ demo/src/main/java/com/example/demo/Ninjas/NinjaMapper.java
@@ -1,4 +1,28 @@
 package com.example.demo.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+ public NinjaModel map(NinjaDTO ninjaDTO){
+     NinjaModel ninjaModel = new NinjaModel();
+     ninjaModel.setId(ninjaDTO.getId());
+     ninjaModel.setNome(ninjaDTO.getNome());
+     ninjaModel.setEmail(ninjaDTO.getEmail());
+     ninjaModel.setIdade(ninjaDTO.getIdade());
+     ninjaModel.setMissoes(ninjaDTO.getMissoes());
+     ninjaModel.setRank(ninjaModel.getRank());
+     return ninjaModel;
+ }
+
+ public NinjaDTO map(NinjaModel ninjaModel){
+     NinjaDTO ninjaDTO = new NinjaDTO();
+     ninjaDTO.setEmail(ninjaModel.getEmail());
+     ninjaDTO.setId(ninjaModel.getId());
+     ninjaDTO.setNome(ninjaModel.getNome());
+     ninjaDTO.setMissoes(ninjaModel.getMissoes());
+     ninjaDTO.setIdade(ninjaModel.getIdade());
+     ninjaDTO.setRank(ninjaModel.getRank());
+     return ninjaDTO;
+ }
 }
